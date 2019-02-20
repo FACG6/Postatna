a81731017c1241c2b43d371e267d83738477e840
AhmedAl-Almi <ahmedalmi95@gmail.com>
2019-02-20 10:19:10 +0200

    hash password

--- src/database/queries/addData.js
+++ src/database/queries/addData.js
@@ -1,10 +1,16 @@
+const bcrypt = require('bcrypt');
 const connect = require('../db_connection');
 
+const salt = bcrypt.genSaltSync(8);
+
+
 const addUser = (userName, userEmail, userPassword, cb) => {
-  const addNewUser = 'insert into users(username,email,password) values($1,$2,$3)'; const values = [userName, userEmail, userPassword];
+  const hashPassword = bcrypt.hashSync(userPassword, salt);
+  const addNewUser = 'insert into users(username,email,password) values($1,$2,$3)'; const values = [userName, userEmail, hashPassword];
   connect.query(addNewUser, values, cb);
 };
-// adduser('ahmed', 'ahmed@hotmail', 'kannjhjh', (err, res) => {
+
+// addUser('ahmed', 'ahmed@hotmail', 'kannjhjh', (err, res) => {
 //   if (err) {
 //     console.log(err);
 //   } else {
